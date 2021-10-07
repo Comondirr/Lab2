ad77a5dee5d53f203ea74cc20cea3e307f624884
chirv <chirv@DESKTOP-VOT2QOU>
2021-10-07 22:40:38 +0200

    Добавьте файлы проекта.

---
+++ Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = double.Parse(Console.ReadLine());
+            double a = Math.Sqrt(Math.Abs(x)) + Math.Exp(Math.Sqrt(b));
+            Console.WriteLine("a={0:F2}", a);
+            double c = Math.Log(a) + Math.Pow(b, 2);
+            Console.WriteLine("c={0:F2}", c);
+            double y = Math.Pow(a, 3) / Math.Cos(c);
+            Console.WriteLine("y={0:F2}", y);
+        }
+    }
+}
